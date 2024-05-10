f32afeac69d688527ea0fbeeaa3a0dba935d9b3a
Constanze <mezocsongor@gmail.com>
2024-05-10 21:59:13 +0200

    fix: removed board generics and added better error handling to the parsing functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use core::panic;
-
 use bevy::{
     math::Vec2,
     prelude::*,
@@ -9,18 +7,17 @@ use bevy::{
 };
 
 const BOARD: &'static str = "
-4003333300400
-0000030000000
-0000000000000
-3000020000300
-3000222000300
-3302212203300
-3000222000300
-3000020000300
-0000000000000
-0000030000000
-4003333300400
-0120012304000
+40033333004
+00000300000
+00000000000
+30000200003
+30002220003
+33022122033
+30002220003
+30000200003
+00000000000
+00000300000
+40033333004
 ";
 
 const STARTING_POSITION: &'static str = "
@@ -65,7 +62,7 @@ a s 10 7
 
 fn main() {
     let board_data = {
-        let structure = BoardData::<11, 12>::parse_structure(BOARD);
+        let (rows, cols, structure) = BoardData::parse_structure(BOARD).unwrap();
 
         let mut colors = HashMap::<u8, Color>::new();
         colors.insert(0, Color::rgb(0.7, 0.7, 0.7));
@@ -74,15 +71,19 @@ fn main() {
         colors.insert(3, Color::rgb(0.4, 0.2, 0.2));
         colors.insert(4, Color::rgb(0.6, 0.2, 0.2));
 
-        let starting_position = BoardData::<11, 12>::parse_arrangement(STARTING_POSITION);
+        let starting_position =
+            BoardData::parse_arrangement(rows, cols, STARTING_POSITION).unwrap();
 
-        BoardData::<11, 12> {
+        BoardData {
+            rows,
+            cols,
             structure,
-            colors,
             starting_position,
             field_size: 50.,
+            field_colors: colors,
             border_width: 4.,
             outer_border_width: 12.,
+            border_color: Color::rgb(0., 0., 0.),
         }
     };
 
@@ -115,13 +116,7 @@ fn main() {
         .insert_resource(board_data)
         .insert_resource(figure_data)
         .insert_resource(clear_color)
-        .add_systems(
-            Startup,
-            (
-                setup,
-                (spawn_board::<11, 12>, setup_board::<11, 12>).chain(),
-            ),
-        )
+        .add_systems(Startup, (setup, (spawn_board, setup_board).chain()))
         .add_systems(Update, close_on_esc)
         .add_event::<CreateBoardEvent>()
         .run();
@@ -131,27 +126,26 @@ fn main() {
 
 /// Data on how exactly to initialize the board.
 #[derive(Resource)]
-struct BoardData<const WIDTH: usize, const HEIGHT: usize> {
-    structure: [[u8; WIDTH]; HEIGHT],
+struct BoardData {
+    rows: usize,
+    cols: usize,
+    structure: Vec<Vec<u8>>,
     starting_position: Vec<Figure>,
-    colors: HashMap<u8, Color>,
     field_size: f32,
+    field_colors: HashMap<u8, Color>,
     border_width: f32,
     outer_border_width: f32,
+    border_color: Color,
 }
 
-#[derive(Resource)]
-struct FigureData {
-    colors: HashMap<Side, Color>,
-    meshes: HashMap<FigureKind, Mesh>,
-}
+impl BoardData {
+    fn parse_structure(data: &str) -> Result<(usize, usize, Vec<Vec<u8>>), &str> {
+        let mut structure: Vec<Vec<u8>> = vec![];
 
-impl<const WIDTH: usize, const HEIGHT: usize> BoardData<WIDTH, HEIGHT> {
-    fn parse_structure(data: &str) -> [[u8; WIDTH]; HEIGHT] {
-        let mut structure: [[u8; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
+        let mut rows = 0;
+        let mut cols = 0;
 
-        let mut row = 0;
-        let mut col = 0;
+        let mut current_row: Vec<u8> = vec![];
         for (i, c) in data.chars().enumerate() {
             if i == 0 && c == '\n' {
                 continue;
@@ -159,21 +153,30 @@ impl<const WIDTH: usize, const HEIGHT: usize> BoardData<WIDTH, HEIGHT> {
 
             if c != '\n' {
                 if c.is_ascii_digit() {
-                    structure[row][col] = c as u8 - '0' as u8;
+                    let digit = c as u8 - '0' as u8;
+                    current_row.push(digit);
                 } else {
-                    panic!("board data should only have numbers and /n")
+                    return Err("board data should only consist of numbers and \\n");
                 }
-                col += 1;
             } else {
-                row += 1;
-                col = 0;
+                if rows == 0 {
+                    cols = current_row.len()
+                } else if current_row.len() != cols {
+                    return Err("board should have consistent row length");
+                }
+
+                structure.push(current_row.clone());
+                current_row.clear();
+                rows += 1;
             }
         }
 
-        return structure;
+        Ok((rows, cols, structure))
     }
 
-    fn parse_arrangement(data: &str) -> Vec<Figure> {
+    fn parse_arrangement(rows: usize, cols: usize, data: &str) -> Result<Vec<Figure>, &str> {
+        let not_enough_tokens = "a figure should be described by 4, space-separated values";
+
         let mut result: Vec<Figure> = vec![];
 
         for line in data.lines() {
@@ -187,20 +190,20 @@ impl<const WIDTH: usize, const HEIGHT: usize> BoardData<WIDTH, HEIGHT> {
                 match side_unparsed {
                     "a" => Side::Attacker,
                     "d" => Side::Defender,
-                    _ => panic!("side described by data should be either a or d"),
+                    _ => return Err("side described by data should be either a or d"),
                 }
             } else {
-                panic!("a figure should be described by 4, space-separated values");
+                return Err(not_enough_tokens);
             };
 
             let kind = if let Some(kind_unparsed) = token_iter.next() {
                 match kind_unparsed {
                     "k" => FigureKind::King,
                     "s" => FigureKind::Soldier,
-                    _ => panic!("kind described by data should be either k or s"),
+                    _ => return Err("kind described by data should be either k or s"),
                 }
             } else {
-                panic!("a figure should be described by 4, space-separated values");
+                return Err(not_enough_tokens);
             };
 
             let x = if let Some(x_unparsed) = token_iter.next() {
@@ -209,7 +212,7 @@ impl<const WIDTH: usize, const HEIGHT: usize> BoardData<WIDTH, HEIGHT> {
                     .expect("position x should be a usize");
                 x_parsed
             } else {
-                panic!("a figure should be described by 4, space-separated values");
+                return Err(not_enough_tokens);
             };
 
             let y = if let Some(y_unparsed) = token_iter.next() {
@@ -218,15 +221,15 @@ impl<const WIDTH: usize, const HEIGHT: usize> BoardData<WIDTH, HEIGHT> {
                     .expect("position y should be a usize");
                 y_parsed
             } else {
-                panic!("a figure should be described by 4, space-separated values");
+                return Err(not_enough_tokens);
             };
 
-            if WIDTH <= x {
-                panic!("x coordinate of figure should be less then the width of the board");
+            if cols <= x {
+                return Err("x coordinate of figure should be less then the width of the board");
             }
 
-            if HEIGHT <= y {
-                panic!("y coordinate of figure should be less then the height of the board");
+            if rows <= y {
+                return Err("y coordinate of figure should be less then the height of the board");
             }
 
             result.push(Figure {
@@ -236,32 +239,42 @@ impl<const WIDTH: usize, const HEIGHT: usize> BoardData<WIDTH, HEIGHT> {
             });
         }
 
-        return result;
+        Ok(result)
     }
+}
+
+#[derive(Resource)]
+struct FigureData {
+    colors: HashMap<Side, Color>,
+    meshes: HashMap<FigureKind, Mesh>,
 }
 
 // COMPONENTS
 
 #[derive(Component)]
-struct Board<const WIDTH: usize, const HEIGHT: usize> {
-    structure: [[u8; WIDTH]; HEIGHT],
+struct Board {
+    rows: usize,
+    cols: usize,
     starting_position: Vec<Figure>,
     field_size: f32,
     border_width: f32,
-    outer_border_width: f32,
     upper_left_field_position: Vec2,
+    end_positions: Vec<BoardPosition>,
 }
 
-impl<const WIDTH: usize, const HEIGHT: usize> Board<WIDTH, HEIGHT> {
-    fn new(data: &BoardData<WIDTH, HEIGHT>, upper_left: Vec2) -> Self {
-        return Self {
-            structure: data.structure,
+impl Board {
+    fn new(data: &BoardData, upper_left: Vec2) -> Self {
+        Self {
+            rows: data.rows,
+            cols: data.cols,
             starting_position: data.starting_position.to_vec(),
             field_size: data.field_size,
             border_width: data.border_width,
-            outer_border_width: data.outer_border_width,
             upper_left_field_position: upper_left,
-        };
+            end_positions: vec![],
+        }
+
+        // TODO properly set end_positions!!!
     }
 }
 
@@ -301,15 +314,15 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn spawn_board<const WIDTH: usize, const HEIGHT: usize>(
-    board_data: Res<BoardData<WIDTH, HEIGHT>>,
+fn spawn_board(
+    board_data: Res<BoardData>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut event: EventWriter<CreateBoardEvent>,
 ) {
-    let width = WIDTH as f32;
-    let height = HEIGHT as f32;
+    let rows = board_data.rows as f32;
+    let cols = board_data.cols as f32;
 
     let field_size = board_data.field_size;
     let border_width = board_data.border_width;
@@ -317,8 +330,8 @@ fn spawn_board<const WIDTH: usize, const HEIGHT: usize>(
 
     let field_offset = field_size + border_width;
 
-    let total_width = field_offset * width + border_width;
-    let total_height = field_offset * height + border_width;
+    let total_width = field_offset * cols + border_width;
+    let total_height = field_offset * rows + border_width;
 
     let offset_x = -(total_width / 2. - field_offset / 2.);
     let offset_y = total_height / 2. - field_offset / 2.;
@@ -330,7 +343,7 @@ fn spawn_board<const WIDTH: usize, const HEIGHT: usize>(
         commands
             .spawn(MaterialMesh2dBundle {
                 mesh: Mesh2dHandle(meshes.add(Rectangle::new(size_x, size_y))),
-                material: materials.add(Color::rgb(0., 0., 0.)),
+                material: materials.add(board_data.border_color),
                 transform: Transform::from_xyz(0., 0., 0.),
                 ..default()
             })
@@ -341,7 +354,7 @@ fn spawn_board<const WIDTH: usize, const HEIGHT: usize>(
         let mut result: Vec<Entity> = vec![];
 
         let color_map: HashMap<u8, Handle<ColorMaterial>> = board_data
-            .colors
+            .field_colors
             .iter()
             .map(|(key, color)| (*key, materials.add(*color)))
             .collect();
@@ -386,7 +399,7 @@ fn spawn_board<const WIDTH: usize, const HEIGHT: usize>(
         commands
             .spawn((
                 SpatialBundle::default(),
-                Board::<WIDTH, HEIGHT>::new(&board_data, upper_left),
+                Board::new(&board_data, upper_left),
             ))
             .id()
     };
@@ -400,8 +413,8 @@ fn spawn_board<const WIDTH: usize, const HEIGHT: usize>(
     event.send(CreateBoardEvent(board));
 }
 
-fn setup_board<const WIDTH: usize, const HEIGHT: usize>(
-    query: Query<&Board<WIDTH, HEIGHT>>,
+fn setup_board(
+    query: Query<&Board>,
     figure_data: Res<FigureData>,
     mut event: EventReader<CreateBoardEvent>,
     mut commands: Commands,
@@ -430,8 +443,15 @@ fn setup_board<const WIDTH: usize, const HEIGHT: usize>(
             let x = figure.board_position.x as f32;
             let y = figure.board_position.y as f32;
 
-            let mesh = mesh_map.get(&figure.kind).unwrap().clone();
-            let material = material_map.get(&figure.side).unwrap().clone();
+            let mesh = mesh_map
+                .get(&figure.kind)
+                .expect("there should be a mesh associated with the specified figure kind")
+                .clone();
+
+            let material = material_map
+                .get(&figure.side)
+                .expect("there should be a material associated with the specified side")
+                .clone();
 
             commands.spawn(MaterialMesh2dBundle {
                 mesh,
