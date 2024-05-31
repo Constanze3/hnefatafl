be07731c74a9f8aa3d9da84a3dca5459f0995865
Constanze <81444666+Constanze3@users.noreply.github.com>
2024-06-01 01:00:58 +0200

    gitignore: delete macros/target directory

--- macros/target/debug/deps/macros-8e4a57942ec578e0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\rust\hnefatafl\macros\target\debug\deps\libmacros-8e4a57942ec578e0.rmeta: src\lib.rs
-
-C:\Users\User\rust\hnefatafl\macros\target\debug\deps\macros-8e4a57942ec578e0.d: src\lib.rs
-
-src\lib.rs:
--- macros/target/debug/deps/macros-ccd7c1010d73338e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\rust\hnefatafl\macros\target\debug\deps\libmacros-ccd7c1010d73338e.rmeta: src\lib.rs
-
-C:\Users\User\rust\hnefatafl\macros\target\debug\deps\macros-ccd7c1010d73338e.d: src\lib.rs
-
-src\lib.rs:
